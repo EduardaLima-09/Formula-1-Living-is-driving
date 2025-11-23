dd02b4442ee81dfa11effd6f5efd8816a3eac807
EduardaLima-09 <maria.alima@sptech.school>
2025-11-23 20:16:43 -0300

    Atualizado lógica e css de algumas páginas

---
+++ web-data-viz-main/src/database/BdFormula1.sql
@@ -0,0 +1,28 @@
+DROP DATABASE Formula1;
+CREATE DATABASE Formula1;
+USE Formula1;
+
+CREATE TABLE usuario (
+  idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(120) NOT NULL,
+  email VARCHAR(150) NOT NULL UNIQUE,
+  senha VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE partida (
+    idPartida INT PRIMARY KEY AUTO_INCREMENT,
+    dataFim DATETIME
+);
+
+CREATE TABLE resultado (
+    idResultado INT PRIMARY KEY AUTO_INCREMENT,
+    totalAcertos INT,
+    pontos INT,
+    totalErros INT,
+    fkPartida INT,
+		FOREIGN KEY (fkPartida) 
+			REFERENCES partida(idPartida),
+	fkUsuario INT,
+		FOREIGN KEY (fkUsuario) 
+			REFERENCES usuario(idUsuario)
+);
