bab9de72158bbee0960525a0b0e4cc4959a70c28
Maria Eduarda Lima <maria.alima@sptech.school>
2025-11-23 20:01:29 -0300

    Create resultadoModel.js

---
+++ web-data-viz/src/models/resultadoModel.js
@@ -0,0 +1,55 @@
+var database = require("../database/config");
+
+function buscarEstatisticasGerais(idUsuario) {
+    console.log("Acessando o model para buscar estatísticas gerais do usuário:", idUsuario);
+    var instrucaoSql = `
+        SELECT 
+            COALESCE(SUM(totalAcertos), 0) AS totalAcertos,
+            COALESCE(SUM(totalErros), 0) AS totalErros
+        FROM resultado
+        WHERE fkUsuario = ${idUsuario};
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function buscarHistoricoPartidas(idUsuario) {
+    console.log("Acessando o model para buscar histórico de partidas do usuário:", idUsuario);
+    var instrucaoSql = `
+        SELECT 
+            r.totalAcertos, 
+            DATE_FORMAT(p.dataFim, '%d/%m/%Y %H:%i') AS dataPartida
+        FROM resultado r
+        JOIN partida p ON r.fkPartida = p.idPartida
+        WHERE r.fkUsuario = ${idUsuario}
+        ORDER BY p.dataFim ASC;  -- Mudado para ASC para ordem cronológica
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function registrarPartida(idUsuario, totalAcertos, totalErros, pontos) {
+    console.log("Acessando o model para registrar partida e resultado...");
+
+    var instrucaoPartida = `
+        INSERT INTO partida (dataFim) VALUES (NOW());
+    `;
+
+    return database.executar(instrucaoPartida).then(resultadoPartida => {
+        const fkPartida = resultadoPartida.insertId;
+
+        var instrucaoResultado = `
+            INSERT INTO resultado (totalAcertos, pontos, totalErros, fkPartida, fkUsuario) 
+            VALUES (${totalAcertos}, ${pontos}, ${totalErros}, ${fkPartida}, ${idUsuario});
+        `;
+
+        console.log("Executando a instrução SQL de Resultado: \n" + instrucaoResultado);
+        return database.executar(instrucaoResultado);
+    });
+}
+
+module.exports = {
+    buscarEstatisticasGerais,
+    buscarHistoricoPartidas,
+    registrarPartida
+};
